ab3809d5eea8b8330de537923f7e53b43384c202
Valeriy Samoylov <valeriy.samojlov@rubius.com>
2021-03-23 17:15:10 +0300

    Добавьте файлы проекта.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write(" Type your name: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Hello " + s);
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
