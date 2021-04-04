1c098aac229a302d673402f44a1c36b1ece753e0
marazzmo <Ozzmosystem@gmail.com>
2021-04-04 12:20:54 +0700

    init

---
+++ MyFirstApp/Task1Stuff.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Point
+    {
+        public string Name { get; }
+        public int X { get; }
+        public int Y { get; }
+        public Point(string name, int x, int y)
+        {
+            this.Name = name;
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    public class Polygon
+    {
+        List<Point> _points = new List<Point>();
+
+        public Polygon(Point p1, Point p2, Point p3)
+        {
+            _points.Add(p1);
+            _points.Add(p2);
+            _points.Add(p3);
+        }
+        public Polygon(Point p1, Point p2, Point p3, Point p4)
+        {
+            _points.Add(p1);
+            _points.Add(p2);
+            _points.Add(p3);
+            _points.Add(p4);
+        }
+        public Polygon(Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            _points.Add(p1);
+            _points.Add(p2);
+            _points.Add(p3);
+            _points.Add(p4);
+            _points.Add(p5);
+        }
+
+        public double Perimeter()
+        {
+            var points = _points;
+            double result = 0;
+            int pointsCount = points.Count - 1;
+            for(int i = 0; i <= pointsCount - 1; i++)
+            {
+                double d = Math.Sqrt((points[i + 1].X - points[i].X) * (points[i + 1].X - points[i].X) + (points[i + 1].Y - points[i].Y) * (points[i + 1].Y - points[i].Y));
+                result = result + d;
+            }
+            result = result + Math.Sqrt((points[pointsCount].X - points[0].X) * (points[pointsCount].X - points[0].X) + (points[pointsCount].Y - points[0].Y) * (points[pointsCount].Y - points[0].Y));
+            return result;
+        }
+    }
+}
---
+++ MyFirstApp/Task2Stuff.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Vector
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+        public static double Scalar(Vector v1, Vector v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+        }
+        public static Vector Multiplication(Vector v1, Vector v2)
+        {
+            return new Vector(v1.Y * v2.Z - v1.Z * v2.Y, v1.Z * v2.X - v1.X * v2.Z, v1.X * v2.Y - v1.Y * v2.X);
+        }
+        public static double Cos(Vector v1, Vector v2)
+        {
+            return Scalar(v1, v2) / (v1.Length() * v2.Length());
+        }
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+        public static Vector Subsract(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+    }
+}
